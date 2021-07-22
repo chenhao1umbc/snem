8425a433f0b68b88da831a52cedff60298b933ee
chenhao1umbc <chenhao1@umbc.edu>
2021-07-22 16:52:19 -0400

    added the test code for FCN

--- debug.py
+++ debug.py
@@ -460,7 +460,7 @@ if True:
         r = sorted(r, reverse=True)
         return r[0]/J
 
-    def nem_func(x, J=3, Hscale=1, Rbscale=100, max_iter=50, lamb=0, seed=0, model='', show_plot=False):
+    def nem_func(x, J=3, Hscale=1, Rbscale=100, max_iter=150, lamb=0, seed=0, model='', show_plot=False):
         if model == '':
             models = torch.load('../../Hpython/data/nem_ss/models/model_4to50_em20_151epoch_1H_100Rb_v1.pt')
         models = torch.load(model)
@@ -535,7 +535,7 @@ if True:
     I = x_all.shape[0]
     res_mse, res_corr = [], []
     for id in range(1,6):
-        location = f'../../Hpython/data/nem_ss/models/model_4to50_em20_151epoch_1H_100Rb_v{id}.pt'
+        location = f'../../Hpython/data/nem_ss/models/model_4to50_21epoch_1H_100Rb_cold_same_M3_v{id}.pt'
         for i in range(I):
             x = torch.from_numpy(x_all[i]).permute(1,2,0)
             MSE, CORR = [], []
@@ -546,22 +546,7 @@ if True:
             res_mse.append(MSE)
             res_corr.append(CORR)
             print(f'finished {i} samples')
-        torch.save((res_mse, res_corr), f'nem_20iter_v{id}.pt')
-
-    res_mse, res_corr = [], []
-    for id in range(1,6):
-        location = f'../../Hpython/data/nem_ss/models/model_4to50_31epoch_1H_100Rb_v{id}.pt'
-        for i in range(I):
-            x = torch.from_numpy(x_all[i]).permute(1,2,0)
-            MSE, CORR = [], []
-            for ii in range(20):  # for diff initializations
-                shat, Hhat, vhat, Rb = nem_func(x, seed=ii, model=location, show_plot=False)
-                MSE.append(mse(vhat, v))
-                CORR.append(corr(vhat.real, v.real))
-            res_mse.append(MSE)
-            res_corr.append(CORR)
-            print(f'finished {i} samples')
-        torch.save((res_mse, res_corr), f'nem_v{id}.pt')
+        torch.save((res_mse, res_corr), f'nem_CNN_v{id}.pt')
 
 #%% plot nem results
     for i in range(1,6):
@@ -708,4 +693,136 @@ if True:
             res.append(abs(P))
         plt.figure()
         plt.plot(res)
+
+#%% Test FCN nem
+import itertools
+class Fcn(nn.Module):
+    def __init__(self, input, output):
+        super().__init__()
+        self.fc = nn.Linear(input , output)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.fc(x)
+        x2 = self.sigmoid(x1)
+        return x2
+
+def mse(vh, v):
+    J = v.shape[-1]
+    r = [] 
+    permutes = list(itertools.permutations(list(range(J))))
+    for jj in permutes:
+        temp = vh[...,jj[0]], vh[...,jj[1]], vh[...,jj[2]]
+        s = 0
+        for j in range(J):
+            s = s + (v[...,j] -temp[j]).norm()**2
+        r.append(s.item())
+    r = sorted(r)
+    return r[0]/J
+
+def corr(vh, v):
+    J = v.shape[-1]
+    r = [] 
+    permutes = list(itertools.permutations(list(range(J))))
+    for jj in permutes:
+        temp = vh[...,jj[0]], vh[...,jj[1]], vh[...,jj[2]]
+        s = 0
+        for j in range(J):
+            s = s + stats.pearsonr(v[...,j].flatten(), temp[j].flatten())[0]
+        r.append(s)
+    r = sorted(r, reverse=True)
+    return r[0]/J
+
+d = sio.loadmat('../data/nem_ss/100_test_all.mat') 
+"x shape of [I,M,N,F], c [I,M,N,F,J], h [I,M,J]"
+x_all, c_all, h_all = d['x'], d['c_all'], d['h_all']
+d = sio.loadmat('../data/nem_ss/v.mat')
+v = torch.tensor(d['v'], dtype=torch.cdouble) # shape of [N,F,J]
+
+def nem_fcn(x, J=3, Hscale=1, Rbscale=100, max_iter=150, lamb=0, seed=0, model='', show_plot=False):
+    if model == '':
+        print('A FCN model is needed')
+        return None
+    models = torch.load(model)
+    for param in models.parameters():
+        param.requires_grad_(False)
+
+    #%% EM part
+    N, F, M = x.shape
+    NF= N*F
+    x = x.cuda()
+
+    torch.torch.manual_seed(seed)        
+    vhat = torch.randn(1, N, F, J).abs().to(torch.cdouble).cuda()
+    Hhat = torch.randn(1, M, J).to(torch.cdouble).cuda()*Hscale
+    Rb = torch.ones(1, M).diag_embed().cuda().to(torch.cdouble)*Rbscale
+    Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+    Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
+    Rx = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb # shape of [N,F,I,M,M]
+    g = torch.rand(1, J, 250).cuda().requires_grad_()
+    optim_gamma = torch.optim.SGD([g], lr= 0.01)
+    ll_traj = []
+
+    for ii in range(max_iter):
+        "E-step"
+        W = Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() @ Rx.inverse()  # shape of [N, F, I, J, M]
+        shat = W.permute(2,0,1,3,4) @ x[...,None]
+        Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - (W@Hhat@Rs.permute(1,2,0,3,4)).permute(2,0,1,3,4)
+        Rsshat = Rsshatnf.sum([1,2])/NF # shape of [I, J, J]
+        Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((1,2))/NF # shape of [I, M, J]
+
+        "M-step"
+        Hhat = Rxshat @ Rsshat.inverse() # shape of [I, M, J]
+        Rb = Rxxhat - Hhat@Rxshat.transpose(-1,-2).conj() - \
+            Rxshat@Hhat.transpose(-1,-2).conj() + Hhat@Rsshat@Hhat.transpose(-1,-2).conj()
+        Rb = Rb.diagonal(dim1=-1, dim2=-2).diag_embed()
+        Rb.imag = Rb.imag - Rb.imag
+
+        # vj = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+        # vj.imag = vj.imag - vj.imag
+        out = torch.randn(vhat.shape, device='cuda', dtype=torch.double)
+        out = models(g)
+        vhat.real = threshold(out.permute(0,2,1).reshape(1,N,F,J))
+        loss = loss_func(vhat, Rsshatnf.cuda())
+        optim_gamma.zero_grad()   
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_([g], max_norm=10)
+        optim_gamma.step()
+        torch.cuda.empty_cache()
+        
+        "compute log-likelyhood"
+        vhat = vhat.detach()
+        ll, Rs, Rx = log_likelihood(x, vhat, Hhat, Rb)
+        ll_traj.append(ll.item())
+        if ii > 3 and abs((ll_traj[ii] - ll_traj[ii-1])/ll_traj[ii-1]) <1e-3:
+            print(f'EM early stop at iter {ii}')
+        if torch.isnan(torch.tensor(ll_traj[-1])) : input('nan happened')
+
+    if show_plot:
+        plt.figure(100)
+        plt.plot(ll_traj,'o-')
+        plt.show()
+        "display results"
+        for j in range(J):
+            plt.figure(j)
+            plt.subplot(1,2,1)
+            plt.imshow(vhat[...,j].cpu().squeeze().real)
+            plt.colorbar()
+    return shat.cpu(), Hhat.cpu(), vhat.cpu().squeeze(), Rb.cpu()
+
+I = x_all.shape[0]
+res_mse, res_corr = [], []
+for id in range(1,6):
+    location = f'../../Hpython/data/nem_ss/models/model_FCN_21epoch_1H_100Rb_cold_same_M3_v{id}.pt'
+    for i in range(I):
+        x = torch.from_numpy(x_all[i]).permute(1,2,0)
+        MSE, CORR = [], []
+        for ii in range(20):  # for diff initializations
+            shat, Hhat, vhat, Rb = nem_fcn(x, seed=ii, model=location, show_plot=True)
+            MSE.append(mse(vhat, v))
+            CORR.append(corr(vhat.real, v.real))
+        res_mse.append(MSE)
+        res_corr.append(CORR)
+        print(f'finished {i} samples')
+
 #%%
