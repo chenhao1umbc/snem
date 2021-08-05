815b7c3795f722ef30e0f5b5c4c494057e1f1b26
chenhao1umbc <chenhao1@umbc.edu>
2021-08-05 14:09:03 -0400

    added code for EM test of toy shift

--- debug.py
+++ debug.py
@@ -836,7 +836,7 @@ if True:
     vj = torch.tensor(d['vj']).to(torch.cdouble)  # shape of [I, N, F, J]
     x = torch.tensor(d['x'])  # shape of [I, M, N, F]
     cj = torch.tensor(d['cj'])  # shape of [I, M, N, F, J]
-    
+
     def corr(vh, v):
         J = v.shape[-1]
         r = [] 
@@ -849,6 +849,116 @@ if True:
             r.append(s)
         r = sorted(r, reverse=True)
         return r[0]/J
+
+    def nem_func(x, J=3, Hscale=1, Rbscale=100, max_iter=150, lamb=0, seed=0, model='', show_plot=False):
+        if model == '':
+            models = torch.load('../../Hpython/data/nem_ss/models/model_4to50_em20_151epoch_1H_100Rb_v1.pt')
+        models = torch.load(model)
+        for j in range(J):
+            models[j].eval()
+            for param in models[j].parameters():
+                    param.requires_grad_(False)
+
+        #%% EM part
+        "initial"        
+        N, F, M = x.shape
+        NF= N*F
+        x = x.cuda()
+
+        torch.torch.manual_seed(seed)        
+        vhat = torch.randn(1, N, F, J).abs().to(torch.cdouble).cuda()
+        Hhat = torch.randn(1, M, J).to(torch.cdouble).cuda()*Hscale
+        Rb = torch.ones(1, M).diag_embed().cuda().to(torch.cdouble)*Rbscale
+        Rxxhat = (x[...,None] @ x[..., None, :].conj()).sum((0,1))/NF
+        Rs = vhat.diag_embed() # shape of [I, N, F, J, J]
+        Rx = Hhat @ Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() + Rb # shape of [N,F,I,M,M]
+        g = torch.rand(J, 1, 4, 4).cuda().requires_grad_()
+        optim_gamma = torch.optim.SGD([g], lr= 0.05)
+        ll_traj = []
+
+        for ii in range(max_iter): # EM iterations
+            "E-step"
+            W = Rs.permute(1,2,0,3,4) @ Hhat.transpose(-1,-2).conj() @ Rx.inverse()  # shape of [N, F, I, J, M]
+            shat = W.permute(2,0,1,3,4) @ x[...,None]
+            Rsshatnf = shat @ shat.transpose(-1,-2).conj() + Rs - (W@Hhat@Rs.permute(1,2,0,3,4)).permute(2,0,1,3,4)
+            Rsshat = Rsshatnf.sum([1,2])/NF # shape of [I, J, J]
+            Rxshat = (x[..., None] @ shat.transpose(-1,-2).conj()).sum((1,2))/NF # shape of [I, M, J]
+
+            "M-step"
+            Hhat = Rxshat @ Rsshat.inverse() # shape of [I, M, J]
+            Rb = Rxxhat - Hhat@Rxshat.transpose(-1,-2).conj() - \
+                Rxshat@Hhat.transpose(-1,-2).conj() + Hhat@Rsshat@Hhat.transpose(-1,-2).conj()
+            Rb = Rb.diagonal(dim1=-1, dim2=-2).diag_embed()
+            Rb.imag = Rb.imag - Rb.imag
+
+            # vj = Rsshatnf.diagonal(dim1=-1, dim2=-2)
+            # vj.imag = vj.imag - vj.imag
+            out = torch.randn(vhat.shape, device='cuda', dtype=torch.double)
+            for j in range(J):
+                out[..., j] = models[j](g[None,j]).exp().squeeze()
+            vhat.real = threshold(out)
+            loss = loss_func(vhat, Rsshatnf.cuda(), lamb=lamb)
+            optim_gamma.zero_grad()   
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_([g], max_norm=1)
+            optim_gamma.step()
+            torch.cuda.empty_cache()
+            
+            "compute log-likelyhood"
+            vhat = vhat.detach()
+            ll, Rs, Rx = log_likelihood(x, vhat, Hhat, Rb)
+            ll_traj.append(ll.item())
+            if torch.isnan(torch.tensor(ll_traj[-1])) : input('nan happened')
+            if ii > 3 and abs((ll_traj[ii] - ll_traj[ii-1])/ll_traj[ii-1]) <1e-3:
+                # print(f'EM early stop at iter {ii}')
+                break
+
+        if show_plot:
+            plt.figure(100)
+            plt.plot(ll_traj,'o-')
+            plt.show()
+            "display results"
+            for j in range(J):
+                plt.figure(j)
+                plt.subplot(1,2,1)
+                plt.imshow(vhat[...,j].cpu().squeeze().real)
+                plt.colorbar()
+        return shat.cpu(), Hhat.cpu(), vhat.cpu().squeeze(), Rb.cpu()
+
+    I = x_all.shape[0]
+    res_mse, res_corr = [], []
+    for id in range(1,6):
+        location = f'../../Hpython/data/nem_ss/models/model_4to50_21epoch_1H_100Rb_cold_same_M3_v{id}.pt'
+        for i in range(I):
+            x = torch.from_numpy(x_all[i]).permute(1,2,0)
+            MSE, CORR = [], []
+            for ii in range(20):  # for diff initializations
+                shat, Hhat, vhat, Rb = nem_func(x, seed=ii, model=location, show_plot=False)
+                MSE.append(mse(vhat, v))
+                CORR.append(corr(vhat.real, v.real))
+            res_mse.append(MSE)
+            res_corr.append(CORR)
+            print(f'finished {i} samples')
+        torch.save((res_mse, res_corr), f'nem_CNN_v{id}.pt')
+
+#%% Test EM on dynamic toy
+    d = sio.loadmat('../data/nem_ss/test100M3_shift.mat')
+    vj_all = torch.tensor(d['vj']).to(torch.cdouble)  # shape of [I, N, F, J]
+    x_all = torch.tensor(d['x']).permute(1,2,0)  # shape of [I, M, N, F]
+    cj = torch.tensor(d['cj'])  # shape of [I, M, N, F, J]
+
+    I = x_all.shape[0]
+    res_mse, res_corr = [], []
+    for i in range(I):
+        CORR = []
+        for ii in range(20):
+            shat, Hhat, vhat, Rb = em_func(x_all[i], seed=0, show_plot=False)
+            plt.figure()
+            plt.imshow(vhat[...,0].real)
+            plt.show()
+            CORR.append(corr(vhat.real, vj_all[i].real))
+        res_corr.append(CORR)
+        print(f'finished {i} samples')
 
 #%% Prepare real data
     "raw data processing"
